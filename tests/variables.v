266d1d56951ad114aa5f52c9cdd495d51050c525
Alhadis <gardnerjohng@gmail.com>
2021-01-12 08:47:19 +1100

    Add patterns for matching strings and variables

---
+++ tests/variables.v
@@ -0,0 +1,24 @@
+struct EOL {
+	name    string
+	literal string
+	escaped string
+}
+
+fn main(){
+	foo := "Foo"
+	bar := 'Bar'
+	cr  := '\r'
+	lf  := "\n"
+	baz := foo + "$cr$lf" + bar + "${cr}${lf}" + "\x2D" + "${cr + lf}"
+	println(baz)
+	
+	// Struct access
+	crlf := EOL {
+		name:    "CRLF"
+		literal: cr + lf
+		escaped: r'\r\n'
+	}
+	
+	abbr := crlf.name
+	println("<${crlf.name == abbr}>")
+}
