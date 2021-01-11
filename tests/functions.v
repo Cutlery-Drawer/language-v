c424c990104316ee6d19a4654ed4f1af06623531
Alhadis <gardnerjohng@gmail.com>
2021-01-12 04:28:24 +1100

    Harden patterns for matching embedded comments

--- tests/functions.v
+++ tests/functions.v
@@ -1,12 +1,27 @@
-fn (mut u User) register() {
+fn (mut u User) register() (int, string) {
 	u.is_registered = true
 }
 
-fn foo
-(text string) {
+// Function header with embedded comments
+fn            /* 1. Function keyword */
+foo           /* 2. Function name */
+(text string) /* 3. Argument list */
+(int, string) /* 4. Multiple return types */
+{
 	eprintln('file: ' + @FILE + ' | line: ' + @LINE + ' | fn: ' + @MOD + '.' + @FN)
 	println(text)
 }
+
+// Function header with nested embedded comments
+/*=================================================
+(1) /* Function */ keyword:          */fn/* ................. /* (1) */
+(2) /* Function's */ name:           */foo/* ................ /* (2) */
+(3) /* Function's */ arguments:      */(text string)/* ...... /* (3) */
+(4) /* Function's */ return type(s): */(int, string)/* ...... /* (4) */
+========================*/{
+	return true
+}
+
 
 fn bar(baz int, names ...
 []
